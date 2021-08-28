b05ce8d166fba3f8fcb604ac2b1699a73e68eabc
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-08-28 16:21:36 +0700

    read open write append files

--- BasicNodeJS/server.js
+++ BasicNodeJS/server.js
@@ -11,6 +11,7 @@ http
     // let q = url.parse(req.url, true).query; // parse use to analyse string
     // let txt = q.yesr + " " + q.month;
     // res.end(txt);
+
     fs.readFile("index.html", function (err, data) {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
@@ -18,3 +19,25 @@ http
     });
   })
   .listen(8000);
+
+fs.appendFile("mynewfile.txt", "Hello new file 1", function (err) {
+  if (err) throw err;
+  console.log("saved");
+});
+
+// fs.open("mynewfile2.txt", "w", function (err, file) {
+//   // w in parameter mean for writing
+//   if (err) throw err;
+//   console.log("saved file 2");
+// });
+
+// fs.writeFile("mynewfile3.txt", "update", function (err) {
+//   // write file will replace data in file
+//   if (err) throw err;
+//   console.log("saved file 3");
+// });
+
+fs.appendFile("mynewfile3.txt", "add text", function (err) {
+  if (err) throw err;
+  console.log("append filed");
+});
